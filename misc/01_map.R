df5336ea26de168cf98088bf190ac0472619c225
MarcinKosinski <m.p.kosinski@gmail.com>
2019-08-27 12:17:54 +0200

    initial commit

---
+++ misc/01_map.R
@@ -0,0 +1,16 @@
+library(magrittr)
+warsaw_wgs84 <- 
+  read.table('data/warsaw_05_wgs84.txt') %>%
+  set_colnames(c('longitude', 'latitude', 'district_n', 'district_name'))
+
+library(ggplot2)
+library(maps)
+library(ggmap)
+library(ggthemes)
+
+ggplot(warsaw_wgs84, aes(x = longitude, y = latitude, col = district_name)) +
+  geom_point() +
+  coord_map() + 
+  theme_minimal() +
+  guides(col = guide_legend(title = 'District')) +
+  labs(title = 'Warsaw')
