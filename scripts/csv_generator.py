ab13ad601ed9506ea92229fa65442c6992400736
Michał <michalmiotk95@gmail.com>
2019-09-26 15:09:35 +0200

    poprawiono generowanie czasu

--- scripts/csv_generator.py
+++ scripts/csv_generator.py
@@ -3,19 +3,16 @@ import re
 
 # functions
 
-def string_creator(digits):
-    output_string = ''
-    for d in digits:
-        output_string+= str(int(d))+' '
-        
-    return output_string[:-1]
+def string_creator(digits, pos):
+    if len(digits) == 1:
+        digits.append(digits[0])        
+    return int(digits[pos])
 
-def get_time_spent(in_string):
+def get_time_spent(in_string, pos):
     if type(in_string) != str:
         return ''
     digits = re.findall(r"\d*\.\d+|\d+", in_string)
     digits = [float(d) for d in digits]
-    #print(digits)
     minutes_positions = [m.start() for m in re.finditer('min', in_string)]
     hours_positions = [m.start() for m in re.finditer('hr', in_string)]
     hours_positions += [m.start() for m in re.finditer('hours', in_string)]
@@ -29,21 +26,29 @@ def get_time_spent(in_string):
         if len(hours_positions):
             for i in range(len(digits)):
                 digits[i] = digits[i]*60.0
-    returned_val = string_creator(digits)
-    #try:
-    #print(in_string, returned_val, type(returned_val) )
-    #assert type(returned_val)=='str'
-    #except AssertionError as e:
-    #    print(in_string, returned_val, type(returned_val), e)
+    returned_val = string_creator(digits, pos)
     return returned_val
+
+def get_time_spent_0(in_string):
+    return get_time_spent(in_string, 0)
+
+def get_time_spent_1(in_string):
+    return get_time_spent(in_string, 1)
 
 popular_times_1 = pd.read_csv('../data/popular_times_1.csv')
 popular_times_2 = pd.read_csv('../data/popular_times_2.csv')
 places = pd.read_csv('../data/places_healthcare.csv')
 popular_times = pd.concat([popular_times_1, popular_times_2])
+print("unikalne", len(popular_times['place_id'].unique()), len(places['place_id'].unique()))
+print(type(popular_times['place_id'].unique()))
+klucze_popular_times = set(popular_times['place_id'].unique().tolist())
+klucze_places = set(places['place_id'].unique().tolist())
+print("cześć wspolna", len(klucze_popular_times.intersection(klucze_places)))
+print("")
 print(len(popular_times))
 data = pd.merge(places, popular_times, how='inner')
 print(len(data))
-data['av_time_spent_minutes'] = data['av_time_spent'].apply(get_time_spent)
 
+data['min_time_spent_minutes'] = data['av_time_spent'].apply(get_time_spent_0)
+data['max_time_spent_minutes'] = data['av_time_spent'].apply(get_time_spent_1)
 data.to_csv("../data/data.csv")
