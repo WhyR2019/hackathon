aa1391f53ab4f66e06f2d993cfb7ef535ce0d50d
karabanb <b.karaban@me.com>
2019-09-26 13:08:11 +0200

    files and scripts form data inspection was added

--- scripts/200_inspect_data.R
+++ scripts/200_inspect_data.R
@@ -3,6 +3,7 @@
 
 library(tidyverse)
 library(inspectdf)
+library(funModeling)
 
 #### loading data ######################################################################################################
 
@@ -12,6 +13,9 @@ raw_data_popular_times_2 <- read_csv('data/popular_times_2.csv')
 
 raw_data_popular_times <- union(raw_data_popular_times_1, raw_data_popular_times_2)
 
+save(raw_data_places, file = 'data/200_raw_data_places.Rdata')
+save(raw_data_popular_times, file = 'data/200_raw_data_popular_times.Rdata')
+
 rm(raw_data_popular_times_1, raw_data_popular_times_2)
 
 ### indentyfying of NA's
@@ -20,3 +24,15 @@ nas_places <- inspect_na(raw_data_places)
 nas_places %>% show_plot()
 nas_times <- inspect_na(raw_data_popular_times)
 nas_times  %>% show_plot()
+
+### indefyfying of intresting categories 
+
+raw_only_healthcare <- raw_data_places %>%
+  filter(type %in% c('doctor', 'pharmacy', 'dentist', 'hospital')) %>%
+  select(-price_level) %>%
+  separate(point,',',into = c('x', 'y')) 
+
+raw_only_healthcare %>% inspect_na()
+
+write.table(raw_only_healthcare, file = 'only_healthcare.csv')
+
